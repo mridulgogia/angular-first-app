0fdc2b50008ead00709cac91810475696527b45f
Mridul Gogia <gogiamridul@gmail.com>
2020-02-21 00:05:29 +0530

    hello

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +7,8 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class AppHeaderComponent implements OnInit {
 
-  @Output() featureSelected = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-
 }
